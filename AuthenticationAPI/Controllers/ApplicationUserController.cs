3079a01aab7bbc43f1e2f6305c12abf6d8761cc3
Samuel Tijani <omalitijani@gmail.com>
2020-09-16 16:16:04 +0100

    Implement post route

--- AuthenticationAPI/Controllers/ApplicationUserController.cs
+++ AuthenticationAPI/Controllers/ApplicationUserController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AuthenticationtAPI.Models;
+using AuthenticationAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace AuthenticationAPI.Controllers
 {
@@ -19,7 +20,29 @@ namespace AuthenticationAPI.Controllers
         public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
-            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        //POST: /api/ApplicationUser/Register
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser() {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
